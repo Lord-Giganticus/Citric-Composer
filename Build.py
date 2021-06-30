1efa54552136414a28b12371a727eba5f94c5ea2
Lord-Giganticus <mrubins64@gmail.com>
2021-06-30 11:02:55 -0600

    Update Build.py

--- Build.py
+++ Build.py
@@ -1,6 +1,8 @@
 import subprocess
 from os import path
 import os
+import shutil
+import sys
 
 home_dir = path.dirname(__file__)
 
@@ -38,3 +40,25 @@ os.chdir(home_dir)
 
 for command in commands:
     subprocess.call(command, shell=True)
+
+os.chdir("Citric Composer\\Citric Composer\\bin\\Release")
+
+subprocess.call('7z a "Citric Composer.zip" *.* -r', shell=True)
+
+shutil.move("Citric Composer.zip", f"{home_dir}\\Citric Composer.zip")
+
+os.chdir(f"{home_dir}\\Koopa Harmony\\Koopa Harmony\\bin\\Release")
+
+subprocess.call('7z a "Koopa Harmony.zip" *.* -r', shell=True)
+
+shutil.move("Koopa Harmony.zip", f"{home_dir}\\Koopa Harmony.zip")
+
+os.chdir(f"{home_dir}\\Retsuko Sound Tool\\Retsuko Sound Tool\\bin\\Release")
+
+subprocess.call('7z a "Retsuko Sound Tool.zip" *.* -r', shell=True)
+
+shutil.move("Retsuko Sound Tool.zip", f"{home_dir}\\Retsuko Sound Tool.zip")
+
+os.chdir(home_dir)
+
+sys.exit(0)
